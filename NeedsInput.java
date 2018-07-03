76dc4d94abea5a7643f881051621eeeea0920c7a
Megan Avery <mavery@indeed.com>
2018-07-03 15:03:27 -0500

    fixing NeedsInput so it works properly

--- NeedsInput.java
+++ NeedsInput.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 class NeedsInput {
 	public static void main(String[] args) {
 		System.out.println("I will print out the input given to me on argument per line");
-		for (String arg: args) {
-			System.out.println(arg);
+
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			System.out.println(scan.next());
 		}
 	}
 }
